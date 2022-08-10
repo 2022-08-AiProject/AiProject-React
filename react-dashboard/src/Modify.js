4e16461999863e1f7b5ab4d4278a3b9a5b480974
yeeun-b <yeeun.park.417@gmail.com>
2022-08-10 15:52:24 +0900

    0810 기본 폼 생성 -예은-

--- react-dashboard/src/Modify.js
+++ react-dashboard/src/Modify.js
@@ -1,5 +1,5 @@
-import {Card, CardBody, CardTitle, CardText, ListGroup, ListGroupItem, Button} from "reactstrap";
-import './Modify';
+import {Card, CardBody, CardTitle, CardText, ListGroup, ListGroupItem, Button, Input} from "reactstrap";
+import './Modify.css';
 import React, {useState} from "react";
 
 
@@ -12,20 +12,26 @@ function Modify(){
                 <CardTitle tag="h5">
                     내 정보 수정
                 </CardTitle>
-            </CardBody>s
+            </CardBody>
             <ListGroup flush>
                 <ListGroupItem>
-                <span id='user-name'>이름</span>
+                <div className="modify-name-div">
+                <span id='modify-name'>이름</span>
                 <span className='bracket'>&#40;</span>
-                <span id='user-id'>testId</span>
+                <span id='modify-id'>testId</span>
                 <span className='bracket'>&#41;</span>
+                </div>
+                <hr />
+                <CardText className='pwchange-text'>비밀번호 변경</CardText>
+                <Input type='text' placeholder='비밀번호 입력' id='pw' className='modify-input'></Input>
+                <Input type='text' placeholder='비밀번호 확인' id='pwcheck' className='modify-input'></Input>
                 </ListGroupItem>
 
                 <ListGroupItem className='menulist-group'>
                 <CardText className="menulist-text">내가 좋아하는 메뉴</CardText>
-                <p className='menulist-menu' id='modify-menu1'>menu1</p>
-                <p className='menulist-menu' id='modify-menu2'>menu2</p>
-                <p className='menulist-menu' id='modify-menu3'>menu3</p>
+                <Input type='text' placeholder='좋아하는 메뉴 입력' className='modify-input'></Input>
+                <Input type='text' placeholder='좋아하는 메뉴 입력' className='modify-input'></Input>
+                <Input type='text' placeholder='좋아하는 메뉴 입력' className='modify-input'></Input>
                 </ListGroupItem>
             </ListGroup>
             <CardBody>

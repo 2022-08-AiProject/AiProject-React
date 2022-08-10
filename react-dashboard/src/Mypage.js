ad716a2eda9818223db352334700aa2f64901a5d
yeeun-b <yeeun.park.417@gmail.com>
2022-08-10 12:26:38 +0900

    0810 이미지 랜덤 출력 기능 구현 -예은-

--- react-dashboard/src/Mypage.js
+++ react-dashboard/src/Mypage.js
@@ -1,7 +1,17 @@
-import {Card, CardBody, CardTitle, CardText, ListGroup, ListGroupItem, CardLink} from "reactstrap";
+import {Card, CardBody, CardTitle, CardText, ListGroup, ListGroupItem, Button} from "reactstrap";
 import './Mypage.css';
+import React, {useState} from "react";
+
 
 function Mypage(){
+    // 이미지 랜덤 출력 기능
+    /* eslint no-restricted-globals: ["off"] */
+    let randomNumber = Math.floor(Math.random() * 10); // 랜덤 숫자
+    const [food, setFood] = useState('./images/food0'+randomNumber+'.jpg') // 기본값 설정
+    onload = function randomImage(){ // 새로고침 시 이미지 변경
+        setFood('./images/food0'+randomNumber+'.jpg');
+    }
+
     return(
         <div className='mypage-div'>
             <Card>
@@ -12,8 +22,8 @@ function Mypage(){
             </CardBody>
             <div className='card-img-div'>
                 <img
-                    alt="Card"
-                    src="./images/lunch.jpg"
+                    alt="음식 이미지"
+                    src={food}
                     className='card-img'
                     />
             </div>
@@ -24,24 +34,21 @@ function Mypage(){
                 <span id='user-id'>testId</span>
                 <span className='bracket'>&#41;</span>
                 </ListGroupItem>
-                <ListGroupItem>
-                A second item
-                </ListGroupItem>
-                <ListGroupItem>
-                And a third item
+
+                <ListGroupItem className='menulist-group'>
+                <CardText className="menulist-text">내가 좋아하는 메뉴</CardText>
+                <p className='menulist-menu'>menu1</p>
+                <p className='menulist-menu'>menu2</p>
+                <p className='menulist-menu'>menu3</p>
                 </ListGroupItem>
             </ListGroup>
             <CardBody>
-                <CardLink href="#">
-                Card Link
-                </CardLink>
-                <CardLink href="#">
-                Another Card Link
-                </CardLink>
+                <Button className='mypage-btn'>수정</Button>
             </CardBody>
             </Card>
         </div>
     );
+    
 };
 
 export default Mypage;

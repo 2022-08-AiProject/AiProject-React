88b4ceaef5504810da66a36bc4b3d0168f0509aa
yeeun-b <yeeun.park.417@gmail.com>
2022-08-10 11:10:28 +0900

    0810 마이페이지 수정 -예은-

--- react-dashboard/src/Mypage.js
+++ react-dashboard/src/Mypage.js
@@ -1,45 +1,45 @@
 import {Card, CardBody, CardTitle, CardText, ListGroup, ListGroupItem, CardLink} from "reactstrap";
+import './Mypage.css';
 
 function Mypage(){
     return(
-        <div>
-            <Card
-  style={{
-    width: '18rem'
-  }}
->
-  <img
-    alt="Card"
-    src="https://picsum.photos/300/200"
-  />
-  <CardBody>
-    <CardTitle tag="h5">
-      Card Title
-    </CardTitle>
-    <CardText>
-      This is some text within a card body.
-    </CardText>
-  </CardBody>
-  <ListGroup flush>
-    <ListGroupItem>
-      An item
-    </ListGroupItem>
-    <ListGroupItem>
-      A second item
-    </ListGroupItem>
-    <ListGroupItem>
-      And a third item
-    </ListGroupItem>
-  </ListGroup>
-  <CardBody>
-    <CardLink href="#">
-      Card Link
-    </CardLink>
-    <CardLink href="#">
-      Another Card Link
-    </CardLink>
-  </CardBody>
-</Card>
+        <div className='mypage-div'>
+            <Card>
+            <CardBody>
+                <CardTitle tag="h5">
+                    마이페이지
+                </CardTitle>
+            </CardBody>
+            <div className='card-img-div'>
+                <img
+                    alt="Card"
+                    src="./images/lunch.jpg"
+                    className='card-img'
+                    />
+            </div>
+            <ListGroup flush>
+                <ListGroupItem>
+                <span id='user-name'>이름</span>
+                <span className='bracket'>&#40;</span>
+                <span id='user-id'>testId</span>
+                <span className='bracket'>&#41;</span>
+                </ListGroupItem>
+                <ListGroupItem>
+                A second item
+                </ListGroupItem>
+                <ListGroupItem>
+                And a third item
+                </ListGroupItem>
+            </ListGroup>
+            <CardBody>
+                <CardLink href="#">
+                Card Link
+                </CardLink>
+                <CardLink href="#">
+                Another Card Link
+                </CardLink>
+            </CardBody>
+            </Card>
         </div>
     );
 };

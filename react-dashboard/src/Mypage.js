b10c8ea87d2dbc85c1af40feeac964ecf95c8b5c
yeeun-b <yeeun.park.417@gmail.com>
2022-08-10 10:22:34 +0900

    0810 마이페이지 카드 추가 -예은-

--- react-dashboard/src/Mypage.js
+++ react-dashboard/src/Mypage.js
@@ -1,7 +1,45 @@
+import {Card, CardBody, CardTitle, CardText, ListGroup, ListGroupItem, CardLink} from "reactstrap";
+
 function Mypage(){
     return(
         <div>
-            Mypage
+            <Card
+  style={{
+    width: '18rem'
+  }}
+>
+  <img
+    alt="Card"
+    src="https://picsum.photos/300/200"
+  />
+  <CardBody>
+    <CardTitle tag="h5">
+      Card Title
+    </CardTitle>
+    <CardText>
+      This is some text within a card body.
+    </CardText>
+  </CardBody>
+  <ListGroup flush>
+    <ListGroupItem>
+      An item
+    </ListGroupItem>
+    <ListGroupItem>
+      A second item
+    </ListGroupItem>
+    <ListGroupItem>
+      And a third item
+    </ListGroupItem>
+  </ListGroup>
+  <CardBody>
+    <CardLink href="#">
+      Card Link
+    </CardLink>
+    <CardLink href="#">
+      Another Card Link
+    </CardLink>
+  </CardBody>
+</Card>
         </div>
     );
 };

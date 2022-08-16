45b6943a360bd4622f9cde32cdfe8d6c939b3bb2
yeeun-b <yeeun.park.417@gmail.com>
2022-08-16 12:19:33 +0900

    0816 로그인확인 알림 추가 -예은-

--- react-dashboard/src/Join.js
+++ react-dashboard/src/Join.js
@@ -48,6 +48,12 @@ function Join() {
       password2: passwordCheck,
     };
 
+     // 유효성 검사
+     if(password !== password2) {
+        alert('비밀번호와 비밀번호 확인이 일치하지 않습니다')
+        return false
+      }
+
     axios
       .post("http://localhost:8000/users/register/", user)
       .then((res) => {
